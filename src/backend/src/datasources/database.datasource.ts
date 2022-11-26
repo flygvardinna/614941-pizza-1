673fc4f6de3e33b55658324b0c6fc87c8f2c6cb5
Oksana Zhuk <oksana.zhuk@noveogroup.com>
2022-11-26 21:21:32 +0700

    update database credentials

--- src/backend/src/datasources/database.datasource.ts
+++ src/backend/src/datasources/database.datasource.ts
@@ -2,23 +2,12 @@ import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
-  name: 'pizza',
   connector: 'postgresql',
-  url: 'postgres://pizza_zl04_user:fCiKgoioruGLYprQ8fsVFacgpoiHYd6C@dpg-ce11o1arrk09esb0on50-a/pizza_zl04',
-  host: 'dpg-ce11o1arrk09esb0on50-a',
-  port: 5432,
-  user: 'pizza_zl04_user',
-  password: 'fCiKgoioruGLYprQ8fsVFacgpoiHYd6C',
-  database: 'pizza_zl04',
-};
-
-/* const config = {
-  connector: 'postgresql',
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgres://oz:pdMeTvwK0jxI22vevcrCaiVQQFp8ukWv@dpg-ce12055a4996ndsm6e60-a/pizza_8zcw',
   ssl: {
     rejectUnauthorized: false
   }
-}; */
+};
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
